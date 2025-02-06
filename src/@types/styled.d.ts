efcb55add4e5dd2b9dc0d4d604533fe0e15d4436
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2025-02-05 22:40:02 -0300

    feat: add styled-components theme definitions

---
+++ src/@types/styled.d.ts
@@ -0,0 +1,27 @@
+import 'styled-components';
+
+declare module 'styled-components' {
+	export interface DefaultTheme {
+		colors: {
+			'yellow-dark': string;
+			yellow: string;
+			'yellow-light': string;
+
+			'purple-dark': string;
+			purple: string;
+			'purple-light': string;
+
+			'base-title': string;
+			'base-subtitle': string;
+			'base-text': string;
+			'base-label': string;
+			'base-hover': string;
+			'base-button': string;
+			'base-input': string;
+			'base-card': string;
+
+			background: string;
+			white: string;
+		};
+	}
+}
