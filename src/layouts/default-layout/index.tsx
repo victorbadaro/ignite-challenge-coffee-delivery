4bf28d6486046d8cd54390614a133e2aaa4b2566
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2025-02-11 07:21:57 -0300

    feat: add a basic default layout component

---
+++ src/layouts/default-layout/index.tsx
@@ -0,0 +1,11 @@
+import { Header } from '@/components/header';
+import { Outlet } from 'react-router-dom';
+
+export function DefaultLayout() {
+	return (
+		<>
+			<Header />
+			<Outlet />
+		</>
+	);
+}
